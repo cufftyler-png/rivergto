588c08484451c05a45901952416860d25e422df1
Tyler Cuff <cuff.tyler@gmail.com>
2026-03-25 18:25:49 -0400

    Temporarily allow all CORS for debugging

--- server/src/index.js
+++ server/src/index.js
@@ -4,38 +4,7 @@ import { solveRoute } from "./routes/solve.js";
 
 const app = express();
 
-const allowedOrigins = [
-  "https://rivergto.com",
-  "https://www.rivergto.com",
-  "http://localhost:5173",
-];
-
-app.use((req, res, next) => {
-  const origin = req.headers.origin;
-
-  if (origin && allowedOrigins.includes(origin)) {
-    res.setHeader("Access-Control-Allow-Origin", origin);
-  }
-
-  res.setHeader("Vary", "Origin");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
-  }
-
-  next();
-});
-
-app.use(
-  cors({
-    origin: allowedOrigins,
-    methods: ["GET", "POST", "OPTIONS"],
-    allowedHeaders: ["Content-Type"],
-  })
-);
-
+app.use(cors());
 app.use(express.json({ limit: "2mb" }));
 
 app.get("/", (_req, res) => {
